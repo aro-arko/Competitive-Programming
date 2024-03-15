f5414626f4727f2ed88880b5a4f959fc54fe3395
Abidur Rahman Arko <aroarko.sd@gmail.com>
2024-03-15 17:47:04 +0800

    A four digits solved

---
+++ Week 1/Day 1/A_Four_Digits.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    cout << setfill('0') << setw(4) << n;
+    return 0;
+}
