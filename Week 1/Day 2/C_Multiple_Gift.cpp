068a2345a217b9b86ae5ba1cc99d06c36633fa4e
Abidur Rahman Arko <aroarko.sd@gmail.com>
2024-03-16 18:28:13 +0800

    I multiple gift solved

---
+++ Week 1/Day 2/C_Multiple_Gift.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll x, y;
+    cin >> x >> y;
+
+    int cnt = 0;
+    for (ll i = x; i <= y; i *= 2)
+    {
+        cnt++;
+    }
+    cout << cnt << '\n';
+    return 0;
+}
