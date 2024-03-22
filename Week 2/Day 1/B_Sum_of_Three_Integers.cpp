44f658339fbf83a7e0abcd140bb1bf05edf75134
Abidur Rahman Arko <aroarko.sd@gmail.com>
2024-03-22 21:21:55 +0800

    B no solved

---
+++ Week 2/Day 1/B_Sum_of_Three_Integers.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int k, s;
+    cin >> k >> s;
+
+    int ans = 0;
+
+    for (int i = 0; i <= k; i++)
+    {
+        for (int j = 0; j <= k; j++)
+        {
+            int z = s - (i + j);
+
+            if (z >= 0 && z <= k)
+            {
+                ans++;
+            }
+        }
+    }
+
+    cout << ans << '\n';
+    return 0;
+}
