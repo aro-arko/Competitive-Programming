e84b5eab634ac8be4a398a11eb0967df3d9f94aa
Abidur Rahman Arko <aroarko.sd@gmail.com>
2024-03-25 04:48:25 +0800

    k bracket solved

---
+++ Week 2/Day 3/K_Move_Brackets.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int total = 0;
+        int flip = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '(')
+            {
+                flip += 1;
+            }
+            else
+            {
+                flip -= 1;
+            }
+
+            if (flip < 0)
+            {
+                total++;
+                flip = 0;
+            }
+        }
+
+        cout << total << '\n';
+    }
+    return 0;
+}
