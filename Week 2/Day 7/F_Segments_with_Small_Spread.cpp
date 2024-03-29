dd2f39d5aed98d879d9a7fe9bd8c13159c888a4b
Abidur Rahman Arko <aroarko.sd@gmail.com>
2024-03-30 06:22:06 +0800

    f segment solved

---
+++ Week 2/Day 7/F_Segments_with_Small_Spread.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    ll k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    ll l = 0, r = 0, ans = 0;
+    multiset<ll> ml;
+
+    while (r < n)
+    {
+        ml.insert(a[r]);
+        ll mn, mx;
+        mn = *ml.begin();
+        mx = *ml.rbegin();
+
+        if ((mx - mn) <= k)
+        {
+            ans += (r - l + 1);
+        }
+        else
+        {
+            while (l < r)
+            {
+                mn = *ml.begin(), mx = *ml.rbegin();
+                if ((mx - mn) <= k)
+                {
+                    break;
+                }
+                auto it = ml.find(a[l]);
+                ml.erase(it);
+                l++;
+            }
+
+            mn = *ml.begin(), mx = *ml.rbegin();
+            if ((mx - mn) <= k)
+            {
+                ans += (r - l + 1);
+            }
+        }
+        r++;
+    }
+
+    cout << ans << '\n';
+    return 0;
+}
