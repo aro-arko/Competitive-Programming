cdfa595ca9d835b7ad52515cf123e9854d0b5281
Abidur Rahman Arko <aroarko.sd@gmail.com>
2024-04-05 09:33:37 +0800

    j no solved

---
+++ Week 3/Day 7/J_Ski_Resort.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, k, q;
+        cin >> n >> k >> q;
+
+        vector<ll> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        ll cons = 0, ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] <= q)
+            {
+                cons++;
+                if (i == n - 1 || a[i + 1] > q)
+                {
+                    if (cons >= k)
+                    {
+                        ll x = cons - k + 1LL;
+                        ll add = (x * (x + 1LL)) / 2LL;
+                        ans += add;
+                    }
+                    cons = 0;
+                }
+            }
+        }
+
+        cout << ans << '\n';
+    }
+    return 0;
+}
