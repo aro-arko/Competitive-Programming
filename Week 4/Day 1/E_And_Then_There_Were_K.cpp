2ac351d7060370b261e53238c850ce9f730930e6
Abidur Rahman Arko <aroarko.sd@gmail.com>
2024-04-18 02:03:05 +0800

    e no solved

---
+++ Week 4/Day 1/E_And_Then_There_Were_K.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        ll n;
+        cin >> n;
+        ll ans = 1;
+        while (ans <= n)
+        {
+            ans *= 2;
+        }
+
+        ans /= 2;
+        ans--;
+        cout << ans << endl;
+    }
+    return 0;
+}
