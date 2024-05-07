8f9055b650572001c9929aa25dba130dcc4393e1
Abidur Rahman Arko <aroarko.sd@gmail.com>
2024-05-07 21:00:26 +0800

    all

---
+++ Week 7/Day 1/D_Paprika_and_Permutation.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int ar[n];
+        map<int, int> mp, mp1;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> ar[i];
+            mp[ar[i]]++;
+        }
+        vector<int> v;
+        for (auto it : mp)
+        {
+            int x = it.first;
+            int y = it.second;
+            if (x <= n)
+            {
+                for (int i = 1; i < y; i++)
+                    v.push_back(x);
+                mp1[x] = 1;
+            }
+            else
+            {
+                for (int i = 1; i <= y; i++)
+                    v.push_back(x);
+            }
+        }
+
+        int cnt = 1;
+        int ans = 0;
+        for (int i = 0; i < v.size(); i++)
+        {
+            for (int j = cnt; j <= n; j++)
+            {
+                if (mp1[j] == 1)
+                    cnt++;
+                else
+                    break;
+            }
+            int x = (v[i] - 1) / 2;
+            if (cnt <= x)
+            {
+                cnt++;
+                ans++;
+            }
+            else
+            {
+                ans = -1;
+                break;
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
